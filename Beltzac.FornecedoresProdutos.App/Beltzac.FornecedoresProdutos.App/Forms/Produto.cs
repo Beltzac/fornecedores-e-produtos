92022c69a0e6590f0ea0efedbe55edae07c33d50
Alexandre Beltzac <beltzac@gmail.com>
2020-09-14 22:35:52 -0300

    Base

---
+++ Beltzac.FornecedoresProdutos.App/Beltzac.FornecedoresProdutos.App/Forms/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Beltzac.FornecedoresProdutos.App
+{
+    public partial class Produto : Form
+    {
+        public Produto()
+        {
+            InitializeComponent();
+        }
+    }
+}
